bd8016263cb74e253d44b967d8a84a242b7b0fa7
RabbitRun <barateligeorge@yandex.ru>
2019-11-12 20:33:21 +0300

    Сверстал слайдер
    Сверстал слайдер, но JavaScript работет некорректно. Надо исправлять

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ var subMenu = document.querySelector(".flex-bottom");
 var search = document.querySelector("input[type='search']");
 var searchButton = document.querySelector("input[name='Найти']");
 var plusButton = document.querySelector(".plus");
+var sliderButton1 = document.querySelector(".slider-button-1");
+var sliderButton2 = document.querySelector(".slider-button-2");
+var sliderButton3 = document.querySelector(".slider-button-3");
+var slide1 = document.querySelector(".js-slide-1");
+var slide2 = document.querySelector(".js-slide-2");
+var slide3 = document.querySelector(".js-slide-3");
+var slides = [slide1, slide2, slide3];
+
 
 buttonCatalog.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -32,4 +40,34 @@ search.addEventListener("blur", function (evt) {
   evt.preventDefault();
   searchButton.classList.remove("js-search-show");
   searchButton.classList.add("js-search-hide");
+});
+
+sliderButton1.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (!slide1.classList.contains("js-slide-show")) {
+    for (var i = 0; i <= slides.length - 1; i++) {
+      slides[i].classList.remove("js-slide-show");
+    }
+    slide1.classList.add("js-slide-show");
+  }
+});
+
+sliderButton2.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (!slide2.classList.contains("js-slide-show")) {
+    for (var i = 0; i <= slides.length - 1; i++) {
+      slides[i].classList.remove("js-slide-show");
+    }
+    slide2.classList.add("js-slide-show");
+  }
+});
+
+sliderButton3.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (!slide3.classList.contains("js-slide-show")) {
+    for (var i = 0; i <= slides.length - 1; i++) {
+      slides[i].classList.remove("js-slide-show");
+    }
+    slide3.classList.add("js-slide-show");
+  }
 });
